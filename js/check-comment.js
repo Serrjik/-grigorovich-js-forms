0513be6209b074cb9a224519eaada39bdbf2a159
Serrjik <uac.sarge@gmail.com>
2018-12-24 18:58:33 +0300

    comment form checking by youtube example -- ready

--- js/check-comment.js
+++ js/check-comment.js
@@ -12,6 +12,8 @@ $(document).ready(function() {
 				_setUpListeners(); // Запускаем прослушку событий
 			}
 
+			// Приватные методы
+
 			// Метод прослушки событий
 			// В нем прослушиваем события на странице, например клики по кнопкам, и при срабатывании события запускаем нужный метод нашего модуля
 			var _setUpListeners = function(){
@@ -21,22 +23,15 @@ $(document).ready(function() {
 				});
 			}
 
-			// Приватные методы
-
 			var _formValidate = function (event) {
-				// event.preventDefault();
 
-				if ( _commentTextarea.val() == '' ) {
-					// _commentErrorEmpty.removeClass('error--hide');
+				if ( _commentTextarea.val().trim() == '' ) {
 					_commentErrorEmpty.fadeIn(1000);
 					event.preventDefault();
 				} else {
 					_commentErrorEmpty.fadeOut(1000);
 					console.log("Hello from _formValidate(), block else");
-					// $('form').unbind('submit').submit();
 				}
-
-				// console.log(_commentTextarea.val());
 
 			}
 
