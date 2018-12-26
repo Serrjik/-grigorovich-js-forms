2ec782e3fc38ef5e12b6b79539c2760ffef73ccc
Serrjik <uac.sarge@gmail.com>
2018-12-26 22:59:52 +0300

    login-form-check - right user email checking ready

--- js/check-login.js
+++ js/check-login.js
@@ -21,12 +21,17 @@ $(document).ready(function() {
 					inputs = form.find('input'),
 					valid = true;
 
+				// Loop through each input field
 				$.each(inputs, function(index, val){
 					var input = $(val),
 						value = input.val().trim(),
 						formGroup = input.parents('.input-form-registration'),
 						noEmailError = $('<div class="notify notify--error mb-20">Введите email</div>'),
-						tooltip = $('<span>Ошибка!</span>');
+						invalidEmailError = $('<div class="notify notify--error mb-20">Неверный формат email</div>'),
+						wrongEmailOrPasswordError = $('<div class="notify no-paddings"><div class="notify no-radius-bottom notify--error">Неверный email или пароль</div><div class="notify no-radius-top"><p>Введите верные данные для входа или воспользуйтесь<a href="#!">восстановлением пароля </a>, чтобы войти на сайт.</p></div></div>'),
+						tooltip = $('<span>Ошибка!</span>'),
+						userEmail = "mail@mail.com",
+						userPassword = "123";
 
 					// console.log("index = " + index);
 					// console.log("val = " + val);
@@ -34,11 +39,41 @@ $(document).ready(function() {
 					// console.log("value = " + value);
 
 					if ( value.length === 0 ){
-						console.log("Here is check of text presence in input field.");
+						// console.log("Here is check of text presence in input field.");
 						console.log("formGroup: " + formGroup);
 						form.find('.notify--error').remove();
 						noEmailError.prependTo(form);
 						valid = false;
+					} else {
+						form.find('.notify--error').remove();
+					}
+
+					console.log("input.attr('type') = " + input.attr('type') );
+
+					if ( input.attr('type').toLowerCase() === 'email' ) {
+						if ( value !== '' ) {
+							// email address matching pattern
+							var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+							if ( pattern.test( value ) ) {
+								form.find('.notify--error').remove();
+								console.log("Email is VALID");
+
+								if ( value !== userEmail ) {
+									form.find('.notify--error').remove();
+									wrongEmailOrPasswordError.prependTo(form);
+									valid = false;
+									console.log("Email is WRONG");
+								} else {
+									form.find('.notify--error').remove();
+									console.log("Email is RIGHT");
+								}
+							} else {
+								form.find('.notify--error').remove();
+								invalidEmailError.prependTo(form);
+								valid = false;
+								console.log("Email is INVALID");
+							}
+						}
 					}
 
 					// Hide errors
@@ -51,6 +86,7 @@ $(document).ready(function() {
 					});
 
 				});
+				// End --- Loop through each input field
 
 				formValidation.isValid = valid;
 
@@ -60,8 +96,9 @@ $(document).ready(function() {
 				console.log('formValidation.isValid = ' + formValidation.isValid);
 				if ( formValidation.isValid === true ) {
 					console.log('Sending form!');
+					form.submit();
 				} else {
-					
+					console.log('Validation FAILED!');
 				}
 			}
 
