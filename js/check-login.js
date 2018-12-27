e0c581baf48a2034b9c4151ad4fc45665cdd44e8
Serrjik <uac.sarge@gmail.com>
2018-12-27 23:43:49 +0300

    start develop login-form-check without each cycle

--- js/check-login.js
+++ js/check-login.js
@@ -18,105 +18,71 @@ $(document).ready(function() {
 			_validateForm: function(event){
 				event.preventDefault();
 				var form = $('#form'),
-					inputs = form.find('input'),
+					email = $('#email').val().trim(),
+					password = $('#password').val().trim(),
+					noEmailError = $('<div id="noEmailError" class="notify notify--error mb-20">Введите email</div>'),
+					noPasswordError = $('<div id="noPasswordError" class="notify notify--error mb-20">Введите пароль</div>'),
+					invalidEmailError = $('<div id="invalidEmailError" class="notify notify--error mb-20">Неверный формат email</div>'),
+					wrongEmailOrPasswordError = $('<div id="wrongEmailOrPasswordError" class="notify no-paddings"><div class="notify no-radius-bottom notify--error">Неверный email или пароль</div><div class="notify no-radius-top"><p>Введите верные данные для входа или воспользуйтесь <a href="#!">восстановлением пароля </a>, чтобы войти на сайт.</p></div></div>'),
+					userEmail = "mail@mail.com",
+					userPassword = "123",
+					// email address matching pattern
+					pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i,
 					valid = true;
 
-				// Loop through each input field
-				$.each(inputs, function(index, val){
-					var input = $(val),
-						value = input.val().trim(),
-						formGroup = input.parents('.input-form-registration'),
-						noEmailError = $('<div class="notify notify--error mb-20">Введите email</div>'),
-						noPasswordError = $('<div class="notify notify--error mb-20">Введите пароль</div>'),
-						invalidEmailError = $('<div class="notify notify--error mb-20">Неверный формат email</div>'),
-						wrongEmailOrPasswordError = $('<div class="notify no-paddings"><div class="notify no-radius-bottom notify--error">Неверный email или пароль</div><div class="notify no-radius-top"><p>Введите верные данные для входа или воспользуйтесь <a href="#!">восстановлением пароля </a>, чтобы войти на сайт.</p></div></div>'),
-						tooltip = $('<span>Ошибка!</span>'),
-						userEmail = "mail@mail.com",
-						userPassword = "123";
+					form.find('.notify').remove();
 
-					// console.log("index = " + index);
-					// console.log("val = " + val);
-					// console.log("input = " + input);
-					// console.log("value = " + value);
-
-					// Is length of text input = 0?
-					if ( value.length === 0 ){
-						console.log("form: " + form);
-						form.find('.notify').remove();
-						switch ( input.attr('type').toLowerCase() ) {
-							case 'email':
-								noEmailError.prependTo(form);
-								break;
-							case 'password':
-								noPasswordError.prependTo(form);
-								break;
-						}
+					// Is E-mail right?
+					if ( email.length === 0 ){
+						noEmailError.prependTo(form);
 						valid = false;
+					} else if ( !(pattern.test( email )) ) {
+						invalidEmailError.prependTo(form);
+						valid = false;
+						console.log("Email is INVALID");
+					} else if ( email !== userEmail ) {
+						wrongEmailOrPasswordError.prependTo(form);
+						valid = false;
+						console.log("Email is WRONG");
 					} else {
-						form.find('.notify').remove();
-						console.log("input.attr('type') = " + input.attr('type') );
+						console.log("Email is RIGHT");
+					}
+					// End  -  Is E-mail right?
 
-						// Is E-mail right?
-						if ( input.attr('type').toLowerCase() === 'email' ) {
-							if ( value !== '' ) {
-								// email address matching pattern
-								var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-								if ( pattern.test( value ) ) {
-									form.find('.notify').remove();
-									console.log("Email is VALID");
-
-									if ( value !== userEmail ) {
-										form.find('.notify').remove();
-										wrongEmailOrPasswordError.prependTo(form);
-										valid = false;
-										console.log("Email is WRONG");
-									} else {
-										form.find('.notify').remove();
-										console.log("Email is RIGHT");
-									}
-								} else {
-									form.find('.notify').remove();
-									invalidEmailError.prependTo(form);
-									valid = false;
-									console.log("Email is INVALID");
-								}
-							}
+					// Is password right?
+					if ( password !== '' ) {
+						if ( password !== userPassword ) {
+							wrongEmailOrPasswordError.prependTo(form);
+							valid = false;
+							console.log("Password is WRONG");
+						} else {
+							console.log("Password is RIGHT");
 						}
-						// End  -  Is E-mail right?
-
-						// Is password right?
-						if ( input.attr('type').toLowerCase() === 'password' ) {
-							if ( value !== '' ) {
-								if ( value !== userPassword ) {
-									form.find('.notify').remove();
-									wrongEmailOrPasswordError.prependTo(form);
-									valid = false;
-									console.log("Password is WRONG");
-								} else {
-									form.find('.notify').remove();
-									console.log("Password is RIGHT");
-								}
-							}
-						}
-						// End  -  Is password right?
-				}
-					// End  -  Is length of text input = 0?
+					}
+					// End  -  Is password right?
 
 					// Hide errors
-					input.on('focus', function(){
-						form.find('.notify').remove();
+					$('#email').on('focus', function(){
+						form.find('#noEmailError', '#invalidEmailError').remove();
 					});
 
-					input.on('keydown', function(){
-						form.find('.notify').remove();
+					$('#email').on('keydown', function(){
+						form.find('#noEmailError', '#invalidEmailError').remove();
 					});
 
-				});
-				// End --- Loop through each input field
+					$('#password').on('focus', function(){
+						form.find('#noPasswordError').remove();
+					});
+
+					$('#password').on('keydown', function(){
+						form.find('#noPasswordError').remove();
+					});
+					// End  -  Hide errors
 
 				formValidation.isValid = valid;
 
 			},
+			// End --- _validateForm()
 
 			_sendEmail: function(){
 				console.log('formValidation.isValid = ' + formValidation.isValid);
